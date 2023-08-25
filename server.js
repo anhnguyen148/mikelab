cb527c550a7161e0057413b1f036fba7f47f2cd4
Tram Anh Nguyen <anhng148@gmail.com>
2023-08-25 09:55:56 -0500

    playing aroung with .gitignore

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
     res.send(`Hello from port ${port}.`);
-})
+});
 
 app.listen(port, () => {
     console.log("Connected");
-})
+});
